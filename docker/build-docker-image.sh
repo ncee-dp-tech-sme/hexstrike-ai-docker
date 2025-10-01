4684ec5326f5fc36b3241479aa94d57357e0b002
Alfonso Alongi <alfonso.alongi.it@gmail.com>
2025-10-02 01:47:32 +0200

    up

--- docker/build-docker-image.sh
+++ docker/build-docker-image.sh
@@ -12,5 +12,7 @@ LOG_FILE="${BUILD_LOG:-build.log}"
 
 echo "[+] Building image with docker | log: ${LOG_FILE}" >&2
 
+# Use --no-cache to avoid using cached layers and ensure a consistent build
+# Use --pull to always attempt to pull a newer version of the base image if available
 docker build --pull -t hexstrike-ai:v6.0 -f docker/Dockerfile . \
   2>&1 | tee "${LOG_FILE}"
