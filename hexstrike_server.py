58b1661c817e2f45ae603ae1a884a07e62792990
Erwin Friethoff <erwin.friethoff@nl.ibm.com>
2026-02-08 19:23:11 +0100

    extended the server timeout to 1200 seconds due to timing out of tool runs

--- hexstrike_server.py
+++ hexstrike_server.py
@@ -6663,7 +6663,7 @@ def setup_logging():
 
 # Configuration (using existing API_PORT from top of file)
 DEBUG_MODE = os.environ.get("DEBUG_MODE", "0").lower() in ("1", "true", "yes", "y")
-COMMAND_TIMEOUT = 300  # 5 minutes default timeout
+COMMAND_TIMEOUT = 1200  # 20 minutes default timeout
 CACHE_SIZE = 1000
 CACHE_TTL = 3600  # 1 hour
 
