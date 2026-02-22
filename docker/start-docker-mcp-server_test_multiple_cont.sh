bc6aec3352e660f94885970c06324c133393e7fb
Erwin Friethoff <erwin.friethoff@nl.ibm.com>
2026-02-22 23:28:58 +0100

    fixed rustscan installation

---
+++ docker/start-docker-mcp-server_test_multiple_cont.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -Eeuo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &>/dev/null && pwd )"
+
+cd $SCRIPT_DIR
+
+# create host-side folders used by docker-compose bind mounts
+mkdir -p \
+  "../logs" \
+  "../data/trivy" \
+  "../data/wpscan" \
+  "../data/nuclei-templates" \
+  "../data/amass" \
+  "../data/msf" \
+  "../data/postgres" \
+  "../data/workspace" \
+
+[ -f "../logs/hexstrike.log" ] || touch "../logs/hexstrike.log"
+
+sudo docker-compose \
+  -f "./docker-compose.yml_defunct" \
+  up --force-recreate 
